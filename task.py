01d705faa9182aeab91ce4ec11c39c9ea49df2e7
hansen1416 <badapplesweetie@gmail.com>
2024-03-17 19:30:05 +0800

    1

--- task.py
+++ task.py
@@ -1,9 +1,11 @@
 import asyncio
 import os
 import json
-from multiprocessing import cpu_count
+import time
+from multiprocessing import cpu_count, Process
 
 import cv2
+import tqdm
 import numpy as np
 from playwright.async_api import async_playwright
 
@@ -13,6 +15,9 @@ ANIM_EULER_DIR = os.path.join(
     os.path.expanduser("~"), "Documents", "video2motion", "anim-json-euler"
 )
 
+VIDEOS_DIR = os.path.join(
+    os.path.expanduser("~"), "Documents", "video2motion", "videos"
+)
 
 HUMANOID_BONES = [
     "Hips",
@@ -64,6 +69,9 @@ def split_array(array, n):
 
 
 def get_longest_track(anim_name):
+    """
+    Get the longest track in the animation.
+    """
 
     with open(os.path.join(ANIM_EULER_DIR, f"{anim_name}.json")) as f:
         tracks = json.load(f)
@@ -83,79 +91,91 @@ def get_longest_track(anim_name):
     return max_len, max_name
 
 
-def anim_video(anim_name, lengest_track_len):
+async def anim_video(anim_name, lengest_track_len):
+    """
+    Create a video for the animation by combining the frames.
+    """
 
-    async def main():
+    elevation = 30
+    azimuth = 0
 
-        elevation = 30
-        azimuth = 0
+    target_video_path = os.path.join(
+        VIDEOS_DIR, f"{anim_name}-{elevation}-{azimuth}.avi"
+    )
 
-        async with async_playwright() as p:
-            browser = await p.chromium.launch()
-            page = await browser.new_page(
-                viewport={"width": 800, "height": 600},
-                screen={"width": 800, "height": 600},
+    if os.path.exists(target_video_path):
+        print(f"Video already exists: {target_video_path}")
+        return
+
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        page = await browser.new_page(
+            viewport={"width": 800, "height": 600},
+            screen={"width": 800, "height": 600},
+        )
+
+        frame_data = []
+
+        for frame_idx in tqdm.tqdm(range(lengest_track_len)):
+
+            await page.goto(
+                f"http://localhost:5173/dors.glb/{anim_name}/{elevation}/{azimuth}/{frame_idx}"
             )
 
-            frame_data = []
+            # Wait until #done are visible
+            await page.locator("#done").wait_for()
 
-            for frame_idx in range(lengest_track_len):
+            # `screenshot_bytes` is a bytes object
+            screenshot_bytes = await page.screenshot(type="jpeg", quality=100)
+            # converts the byte string data into a NumPy array with uint8 data type
+            nparr = np.frombuffer(screenshot_bytes, np.uint8)
+            # decodes into an OpenCV format, cv2.IMREAD_COLOR indicating to load a color image.
+            img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+            # print(img_np.shape)  # (720, 1280, 3)
 
-                await page.goto(
-                    f"http://localhost:5173/dors.glb/{anim_name}/{elevation}/{azimuth}/{frame_idx}"
-                )
+            # save img to local file
+            # cv2.imwrite(f"frames\{anim_name}_{frame_idx}.jpg", img_np)
 
-                # Wait until #done are visible
-                await page.locator("#done").wait_for()
+            frame_data.append(img_np)
 
-                # `screenshot_bytes` is a bytes object
-                screenshot_bytes = await page.screenshot(type="jpeg")
-                # converts the byte string data into a NumPy array with uint8 data type
-                nparr = np.frombuffer(screenshot_bytes, np.uint8)
-                # decodes into an OpenCV format, cv2.IMREAD_COLOR indicating to load a color image.
-                img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-                # print(img_np.shape)  # (720, 1280, 3)
+        await browser.close()
 
-                # save img to local file
-                # cv2.imwrite(f"frames\{anim_name}_{frame_idx}.jpg", img_np)
+        # save the frames as a video
+        out = cv2.VideoWriter(
+            target_video_path,
+            cv2.VideoWriter_fourcc(*"DIVX"),
+            50,
+            (800, 600),
+        )
 
-                frame_data.append(img_np)
+        for i in range(len(frame_data)):
+            out.write(frame_data[i])
 
-                if frame_idx % 10 == 0 or frame_idx == lengest_track_len - 1:
-                    print(f"Frame {frame_idx} done.")
+        out.release()
 
-            await browser.close()
+        print(f"Video saved: {target_video_path}")
 
-            # save the frames as a video
-            out = cv2.VideoWriter(
-                os.path.join("videos", f"{anim_name}-{elevation}-{azimuth}.avi"),
-                cv2.VideoWriter_fourcc(*"DIVX"),
-                50,
-                (800, 600),
-            )
 
-            for i in range(len(frame_data)):
-                out.write(frame_data[i])
+class GenerateVideoTask(Process):
 
-            out.release()
+    def __init__(self, anim_tasks, queue_idx) -> None:
+        Process.__init__(self)
 
-    asyncio.run(main())
+        self.anim_tasks = anim_tasks
+        self.queue_idx = queue_idx
+
+    def run(self) -> None:
+
+        for anim_name, max_len in self.anim_tasks:
+            print(f"{self.queue_idx} Processing: {anim_name}, {max_len} frames.")
+
+            asyncio.run(anim_video(anim_name, max_len))
 
 
 if __name__ == "__main__":
 
-    counter = 0
-    limit = 1
-
-    anim_names = []
-
     # list all files in the directory `anim_eulers`
-    for f in os.listdir(ANIM_EULER_DIR):
-        anim_names.append(f.replace(".json", ""))
-        counter += 1
-
-        if counter >= limit:
-            break
+    anim_names = [f.replace(".json", "") for f in os.listdir(ANIM_EULER_DIR)]
 
     print(f"Total number of animations: {len(anim_names)}")
 
@@ -178,23 +198,29 @@ if __name__ == "__main__":
     # get number of cpu cores
     anim_task_arr = split_array(long_anim, cpu_count())
 
-    for anim_task in anim_task_arr:
-        for anim_name, max_len in anim_task:
+    processes = [
+        GenerateVideoTask(anim_tasks=tasks, queue_idx=idx)
+        for idx, tasks in enumerate(anim_task_arr)
+    ]
 
-            print(f"Processing: {anim_name}, {max_len} frames.")
+    start_time = time.time()
 
-            anim_video(anim_name, max_len)
+    # run the process,
+    for process in processes:
+        process.start()
 
-    # async def main():
-    #     async with async_playwright() as p:
-    #         browser = await p.chromium.launch()
-    #         page = await browser.new_page()
-    #         await page.goto("http://localhost:5173/dors.glb/Zombie%20Kicking/10/30/0")
-    #         print(await page.title())
+    for process in processes:
+        # report the daemon attribute
+        print(
+            process.daemon,
+            process.name,
+            process.pid,
+            process.exitcode,
+            process.is_alive(),
+        )
 
-    #         screenshot_bytes = await page.screenshot()
-    #         print(base64.b64encode(screenshot_bytes).decode())
+        process.join()
 
-    #         await browser.close()
+    end_time = time.time()
 
-    # asyncio.run(main())
+    print(f"Time taken: {end_time - start_time}")
